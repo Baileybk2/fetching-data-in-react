b9652a1c09d14f63cb29e588ecd07e5e19061ba6
BaileyBeck <baileybk2@gmail.com>
2024-07-19 10:07:09 -0400

    built search form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import * as weatherService from "./services/weatherService"
+
 const App = () => {
-  return <h1>Hello world!</h1>
+  const fetchData = async () => {
+    const data = await weatherService.show("Boston")
+    console.log("Data:", data)
+  }
+
+  return (
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
+  )
 }
 
 export default App
